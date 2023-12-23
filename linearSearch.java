57cdf64f393c721f95fd1d5982d3bb5a8e4bcf5c
Siddhant Gujrathi <115349105+SiddhantGujrathi1@users.noreply.github.com>
2023-12-23 16:18:30 +0530

    Create linearSearch.java

---
+++ linearSearch.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Primitive {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = {1,5,9,21,14};
+        System.out.println(linearSearch(arr));
+        }
+
+        static int linearSearch(int[] arr){
+        int p = 0;
+            for (int a:arr) {
+                if(a==14){
+                    System.out.println();
+                    return p;
+                }
+                p++;
+            }
+
+            return -1;
+        }
+
+
+}
